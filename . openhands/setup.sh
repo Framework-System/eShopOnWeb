8a7c730038b3712f398f05c0d01b1e10208c43b1
Rafael Gontijo <rafaelgontijowinter@gmail.com>
2025-07-15 15:31:59 -0300

    Adicionar scripts de instalação e diretrizes de documentação

---
+++ . openhands/setup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+apt-get update && apt-get install -y curl \
+                                    libc6 \
+                                    libgcc-s1 \
+                                    libgssapi-krb5-2 \
+                                    libicu72 \
+                                    libssl3 \
+                                    libstdc++6 \
+                                    zlib1g 
+curl -L https://dot.net/v1/dotnet-install.sh -o dotnet-install.sh
+chmod +x ./dotnet-install.sh && ./dotnet-install.sh --channel 2.2
+export PATH=$PATH:/root/.dotnet
+echo "export PATH=\$PATH:/root/.dotnet" >> /root/.bashrc
